8e7e3f0b281470e97835f2b2bd23f3e5e8234914
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-05-18 19:00:24 +0530

    fix pv2 page sidebar

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -30,9 +30,11 @@ const SideBar = ({ sidebarContentDataArray }) => {
     const currentIndex = sidebarContentDataArray.findIndex(
       (item) => item.name === selectedContent
     );
-    const previousIndex = name;
-    currentIndex === 0 ? sidebarContentDataArray.length - 1 : currentIndex - 1;
-    setSelectedContent(sidebarContentDataArray[previousIndex].title);
+    const previousIndex =
+      currentIndex === 0
+        ? sidebarContentDataArray.length - 1
+        : currentIndex - 1;
+    setSelectedContent(sidebarContentDataArray[previousIndex].name);
   };
 
   const handleNext = () => {
@@ -48,9 +50,25 @@ const SideBar = ({ sidebarContentDataArray }) => {
 
   const mainRef = useRef(null);
 
+  const [sidebarTop, setSidebarTop] = useState(150);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setSidebarTop(0);
+    } else {
+      setSidebarTop(150); 
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <div className="lg:hidden absolute top-[4rem] md:top-[6.8rem] left-[1.2rem] md:left-[2.5rem] z-50">
+      <div className="lg:hidden  top-[4rem] md:top-[6.8rem] left-[1.2rem] md:left-[2.5rem] z-50 fixed">
         <span className="hidden md:flex lg:hidden">
           <Hamburger
             toggled={isSidebarVisible}
@@ -67,21 +85,21 @@ const SideBar = ({ sidebarContentDataArray }) => {
         </span>
       </div>
       <div
-        className={` bg-white w-[70vw] sm:w-[450px] flex gap-2 -ml-[1.2rem] md:-ml-[2.5rem] lg:-ml-[4rem] xl:-ml-[6.2rem] shadow-md rounded-br-3xl absolute lg:relative transition-transform duration-500 ease-in-out ${
+        className={`bg-white w-[70vw] sm:w-[450px] flex overflow-hidden gap-2 -ml-[1.2rem] md:-ml-[2.5rem] lg:-ml-[4rem] xl:-ml-[6.2rem] shadow-md rounded-br-3xl absolute lg:fixed transition-transform duration-500 ease-in-out ${
           isSidebarVisible ? "" : "-translate-x-full lg:-translate-x-0"
         }`}
+        style={{ top: `${sidebarTop}px`, transition: "top 0.5s ease" }}
         ref={mainRef}
       >
         <ProgressBar target={mainRef} />
-        <div className="flex flex-col gap-2 text-white font-medium py-10 md:py-20 text-[14px] md:text-[16px] ">
+        <div className="flex flex-col gap-2 text-white font-medium py-10 md:py-20 text-[14px] md:text-[16px] sticky top-0">
           {sidebarContentDataArray.map((item, index) => (
             <span
               key={index}
               onClick={() => {
                 handleClick(item.name);
-                // toggleSidebar();
               }}
-              className={` leading-[21.6px] h-[80px] px-2 md:px-4 py-2 md:py-4 rounded-r-full flex justify-between items-center cursor-pointer ${
+              className={`leading-[21.6px] h-[80px] px-2 md:px-4 py-2 md:py-4 rounded-r-full flex justify-between items-center cursor-pointer ${
                 selectedContent === item.name
                   ? "bg-[#56E9EE] text-black"
                   : "bg-white text-black"
@@ -98,50 +116,52 @@ const SideBar = ({ sidebarContentDataArray }) => {
         </div>
       </div>
 
-      <div className="lg:ml-5 w-full min-h-screen flex flex-col justify-between">
-        {sidebarContentDataArray
-          .filter((content) => content.name === selectedContent)
-          .map((content, index) => (
-            <SideBarContent key={index} content={content} />
-          ))}
-        <span className="flex justify-between lg:pl-8 xl:pl-20">
-          <button
-            onClick={handlePrevious}
-            disabled={
-              sidebarContentDataArray.findIndex(
-                (item) => item.name === selectedContent
-              ) === 0
-            }
-            className={`text-white text-[14px] md:text-[20px] mt-8 md:mt-14 px-5 md:px-9 py-2 md:py-2.5 rounded-3xl bg-gradient-to-r from-[#56E9EE] to-[#016E92] ${
-              sidebarContentDataArray.findIndex(
-                (item) => item.name === selectedContent
-              ) === 0
-                ? "cursor-not-allowed"
-                : ""
-            }`}
-          >
-            Previous
-          </button>
-          <button
-            onClick={handleNext}
-            disabled={
-              sidebarContentDataArray.findIndex(
-                (item) => item.name === selectedContent
-              ) ===
-              sidebarContentDataArray.length - 1
-            }
-            className={`text-white text-[14px] md:text-[20px] mt-8 md:mt-14 px-5 md:px-9 py-2 md:py-2.5 rounded-3xl bg-gradient-to-r from-[#56E9EE] to-[#016E92] ${
-              sidebarContentDataArray.findIndex(
-                (item) => item.name === selectedContent
-              ) ===
-              sidebarContentDataArray.length - 1
-                ? "cursor-not-allowed"
-                : ""
-            }`}
-          >
-            Next
-          </button>
-        </span>
+      <div className="flex lg:ml-[25rem] xl:ml-[21rem]">
+        <div className="lg:ml-5 lg:w-[50vw] xl:w-[60vw] min-h-screen flex flex-col ">
+          {sidebarContentDataArray
+            .filter((content) => content.name === selectedContent)
+            .map((content, index) => (
+              <SideBarContent key={index} content={content} />
+            ))}
+          <span className="flex justify-between lg:pl-8 xl:pl-20">
+            <button
+              onClick={handlePrevious}
+              disabled={
+                sidebarContentDataArray.findIndex(
+                  (item) => item.name === selectedContent
+                ) === 0
+              }
+              className={`text-white text-[14px] md:text-[20px] mt-8 md:mt-14 px-5 md:px-9 py-2 md:py-2.5 rounded-3xl bg-gradient-to-r from-[#56E9EE] to-[#016E92] ${
+                sidebarContentDataArray.findIndex(
+                  (item) => item.name === selectedContent
+                ) === 0
+                  ? "cursor-not-allowed"
+                  : ""
+              }`}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              disabled={
+                sidebarContentDataArray.findIndex(
+                  (item) => item.name === selectedContent
+                ) ===
+                sidebarContentDataArray.length - 1
+              }
+              className={`text-white text-[14px] md:text-[20px] mt-8 md:mt-14 px-5 md:px-9 py-2 md:py-2.5 rounded-3xl bg-gradient-to-r from-[#56E9EE] to-[#016E92] ${
+                sidebarContentDataArray.findIndex(
+                  (item) => item.name === selectedContent
+                ) ===
+                sidebarContentDataArray.length - 1
+                  ? "cursor-not-allowed"
+                  : ""
+              }`}
+            >
+              Next
+            </button>
+          </span>
+        </div>
       </div>
     </>
   );
